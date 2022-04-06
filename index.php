b25b3e90df6986d7e9f8e75dba01923b1b364373
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-06 08:31:00 -0400

    summation function

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 function abbrevName($name)
 {
   $names = explode(' ', ucwords($name));
-  
   return $names[0][0] . '.' . $names[1][0];
 }
 
-var_dump(abbrevName("hugo suarez"));
+function summation($n){
+  return array_sum(range(1,$n));
+}
+
+print_r(summation(8));
