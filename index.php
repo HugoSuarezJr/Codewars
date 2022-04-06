eab9f32cf332a86f9c8c70fd1ac321af0df2ecd1
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-04-06 08:43:56 -0400

    Update comments

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 
-// Abbreviates name into initials. e.g. Hugo Suarez => H.S
+// Abbreviates name into initials (e.g., Hugo Suarez => H.S).
 function abbrevName($name)
 {
   $separateNames = explode(' ', ucwords($name));
   return $separateNames[0][0] . '.' . $separateNames[1][0];
 }
 
-// Returns the summation of a given number. e.g. 4 => 1+2+3+4 = 10
+// Returns the summation of a given number (e.g., 4 => 1+2+3+4 = 10).
 function summation($n){
   return array_sum(range(1,$n));
 }
-
-print_r(summation(4));
