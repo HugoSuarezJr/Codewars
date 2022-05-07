c37545e8d86e39cea9597d7e71dc4043e7a4f7f1
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-05-07 00:16:24 -0400

    isIsogram function, failed to solve but learned about array_unique method

--- index.php
+++ index.php
@@ -193,7 +193,18 @@ function findMissing($list) {
 }
 
 
-echo findMissing($alist);
+function isIsogram($string) {
+  if(empty($string)){
+    return true;
+  }
+
+  $array = str_split(strtolower($string));
+  return (count($array) === count(array_unique($array)));
+}
+
+$stringsss= "jlk smof";
+
+var_dump(isIsogram($stringsss));
 
 
 /**
